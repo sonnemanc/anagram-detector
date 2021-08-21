e2415f026933547882b314988d9480331b78104e
Corban <sonnemanc@gmail.com>
2021-08-21 10:41:38 -0600

    Done with Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(words)
+        words.select {|word| word.split("").sort == @word.split("").sort}
+    end
+end
